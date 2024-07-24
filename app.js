a39542bbd7feb8a43049d044122e95e9b8fcea28
Blackfox2012 <teamfr2@gmail.com>
2024-07-24 23:12:39 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const tg = window.Telegram.WebApp;
 
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document loaded");
     showTab('stock'); // Default to 'Cars in Stock' tab
     displayUserInfo(); // Display user info on page load
 
     tg.onEvent('mainButtonClicked', function() {
+        console.log("Main button clicked");
         if (selectedCar) {
+            console.log("Sending data: ", selectedCar);
             tg.sendData(selectedCar); // send selected car data to the bot
         }
     });
+
+    // Additional logging for initialization
+    console.log("Telegram WebApp initialized: ", tg.initDataUnsafe);
 });
 
 let selectedCar = '';
@@ -19,12 +25,14 @@ function showTab(tabName) {
         tabContent[i].style.display = "none";
     }
     document.getElementById(tabName).style.display = "block";
+    console.log("Showing tab: ", tabName);
 }
 
 function orderCar(carName) {
     selectedCar = carName;
     tg.MainButton.setText(`Order ${carName}`);
     tg.MainButton.show();
+    console.log("Car selected: ", carName);
 }
 
 function displayUserInfo() {
@@ -32,4 +40,5 @@ function displayUserInfo() {
     const p = document.createElement("p");
     p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
     usercard.appendChild(p);
+    console.log("User info displayed: ", tg.initDataUnsafe.user);
 }
